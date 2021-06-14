ae37162273c5377a86980df56bcdc908c749192b
Aman Singhal <85450607+Aman8923@users.noreply.github.com>
2021-06-14 22:23:07 +0530

    Add files via upload

---
+++ Intro Class-2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  8 00:42:12 2021
+
+@author: RAHUL
+"""
+
+X=[1,2,3,4,5]
+Y=[2,4,6,8,10]
+print(X)
+print(Y)
+import matplotlib.pyplot as plt
+plt.plot(X,Y)
+
+   
